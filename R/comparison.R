991fc3f40f6d5d028c8c68b1db15963754b3f5a7
Jesse Sadler <jsadler@ucla.edu>
2019-05-21 01:49:27 -0700

    Equality and comparison for deb_lsd
    * Equality using deb_normalize
    * Comparison by casting to double

---
+++ R/comparison.R
@@ -0,0 +1,20 @@
+## Equality and comparison for
+
+
+# deb_lsd equality --------------------------------------------------------
+
+# vec_proxy_equal is just vec_proxy in GitHub version
+vec_proxy_equal.deb_lsd <- function(x) {
+  x <- deb_normalize(x)
+  data.frame(l = vctrs::field(x, "l"),
+             s = vctrs::field(x, "s"),
+             d = vctrs::field(x, "d"))
+}
+
+
+# deb_lsd comparison ------------------------------------------------------
+
+vec_proxy_compare.deb_lsd <- function(x) {
+  vctrs::field(x, "l") + vctrs::field(x, "s") /
+    deb_bases(x)[[1]] + vctrs::field(x, "d") / prod(deb_bases(x))
+}
